a25b7afe3f6a514a20c0392f87dbfa8f25286632
dengfuping <fuping.dfp@antgroup.com>
2024-09-26 22:19:51 +0800

    feat: deploy to .zeabur

---
+++ scripts/pre-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r dist/* .zeabur/output/static && cp -r api .zeabur/output/functions
+
+for name in `ls -R .zeabur/output/functions/api/v1/car_orders/*.js`
+do
+  mv $name ${name%}.func
+done
