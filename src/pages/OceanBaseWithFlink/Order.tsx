dc2c17651c3602f488edcfb699c5297646740b36
dengfuping <fuping.dfp@antgroup.com>
2024-10-19 22:01:01 +0800

    fix: Remove redundant car image for order page

--- src/pages/OceanBaseWithFlink/Order.tsx
+++ src/pages/OceanBaseWithFlink/Order.tsx
@@ -6,13 +6,6 @@ interface OrderProps {}
 const Order: React.FC<OrderProps> = () => {
   return (
     <>
-      <img
-        src="https://mdn.alipayobjects.com/huamei_fhnyvh/afts/img/A*0-9HSY7fbvMAAAAAAAAAAAAADmfOAQ/original"
-        style={{
-          width: '100%',
-          borderRadius: '0px 0px 20px 20px',
-        }}
-      />
       <OrderForm style={{ padding: '2em' }} />
     </>
   );
